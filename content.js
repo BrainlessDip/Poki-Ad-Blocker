5bbd8835e8c10c314f2b617bab43905d5f799c8a
Dip Dey <dipdey993@gmail.com>
2025-06-18 13:42:14 +0600

    release: Poki Ad Blocker v1.0.0
    - Block ads on Poki.com
    - Automatically skip video

---
+++ content.js
@@ -0,0 +1,40 @@
+window.addEventListener("load", () => {
+  console.log("[Poki Ad Blocker] Script loaded and monitoring ads...");
+  console.log("Poki Ad Blocker @ Brainless Dip");
+  const elements = document.querySelectorAll("div");
+
+  let adActive = false;
+
+  setInterval(() => {
+    const isAdPlaying = document.body.innerText.includes("Playing Ad");
+
+    if (isAdPlaying !== adActive) {
+      adActive = isAdPlaying;
+
+      const adContainer = document.getElementById("poki-video-ad-container");
+      const mediaElements = document.querySelectorAll("video, audio");
+
+      if (adContainer) {
+        adContainer.style.visibility = "hidden";
+      }
+       for (const el of elements) {
+    if (el.textContent.trim() === "We’ll be back after this short break") {
+      el.textContent = "Poki Ad Blocker @ Brainless Dip";
+      break;
+    }
+  }
+      mediaElements.forEach((media) => {
+        media.muted = adActive;
+
+        if (adActive && Number.isFinite(media.duration)) {
+          try {
+            media.currentTime = media.duration;
+            console.log("[Poki Ad Blocker] Ad bypassed. Reward claimed.");
+          } catch (error) {
+            console.error("[Poki Ad Blocker] Error skiping ad", error);
+          }
+        }
+      });
+    }
+  }, 1);
+});
